dcd55c134a6daf486562b7112f98b4118cdcb761
David Restrepo <drestr30@eafit.edu.co>
2025-05-28 21:12:39 -0400

    adding suspense to pdf/page

--- app/listpdf/page.tsx
+++ app/listpdf/page.tsx
@@ -1,6 +1,12 @@
 import dynamic from 'next/dynamic'
 import ListPdfClient from './listpdfclient'
+import { Suspense } from 'react'
 
 export default function Page() {
-  return <ListPdfClient />
+  return (
+    // You could have a loading skeleton as the `fallback` too
+    <Suspense>
+      <ListPdfClient />
+    </Suspense>
+  )
 }
